08fa727bb2b52dd440a60b6ff8e89e90da1c5e37
Ian Douglas <iandouglas@gmail.com>
2015-03-20 14:23:59 -0700

    tagging version 1.0.0
    note that there is no readme or anything like that yet

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'iandouglas:accounts-ui-semantic-ui',
-	version: '0.0.1',
+	version: '1.0.0',
 	// Brief, one-line summary of the package.
 	summary: '',
 	// URL to the Git repository containing the source code for this package.
